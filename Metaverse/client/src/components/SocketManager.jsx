f96a2ae975cef112121f7898b1d58ac1e5daa0d8
Sunil0881 <sunil12162003@gmail.com>
2024-11-03 14:28:59 +0530

    secure added

--- Metaverse/client/src/components/SocketManager.jsx
+++ Metaverse/client/src/components/SocketManager.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { io } from "socket.io-client";
 
 export const socket = io(
-  import.meta.env.VITE_SERVER_URL || "http://195.35.20.212:3000"
+  import.meta.env.VITE_SERVER_URL || "http://starkshoot.fun:3000", { secure: false }
 );
 export const charactersAtom = atom([]);
 export const mapAtom = atom(null);
